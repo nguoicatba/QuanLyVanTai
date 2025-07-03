3d30df2ec34c90f1e75f9f46e6c3818c7eb5b159
Nguyễn Lê Trung Kiên <thanheco1978@gmail.com>
2025-07-03 08:57:08 +0700

    add: language for button

---
+++ resources/lang/en/button.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'view' => 'View',
+    'save' => 'Save',
+    'cancel' => 'Cancel',
+    'search' => 'Search',
+    'reset' => 'Reset',
+    'back' => 'Back',
+];
---
+++ resources/lang/vi/button.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'create' => 'Tạo mới',
+    'edit' => 'Chỉnh sửa',
+    'delete' => 'Xóa',
+    'view' => 'Xem',
+    'save' => 'Lưu',
+    'cancel' => 'Hủy bỏ',
+    'search' => 'Tìm kiếm',
+    'reset' => 'Đặt lại',
+    'back' => 'Quay lại',
+];
